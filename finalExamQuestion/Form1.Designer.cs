3a4804d09325fd1958decad80907c5165efc2bfb
TheCollegeProf <keith.connell@georgiancollege.ca>
2018-12-10 09:21:20 -0500

    first commit

---
+++ finalExamQuestion/Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace finalExamQuestion
+{
+    partial class contactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtStreetNumber = new System.Windows.Forms.TextBox();
+            this.txtStreetName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.cmbStreetType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbProvinces = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.chkPermission = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtPasswordVerify = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(99, 537);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(150, 45);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(275, 537);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(150, 45);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(30, 49);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(157, 30);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtStreetNumber
+            // 
+            this.txtStreetNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStreetNumber.Location = new System.Drawing.Point(30, 113);
+            this.txtStreetNumber.Name = "txtStreetNumber";
+            this.txtStreetNumber.Size = new System.Drawing.Size(61, 30);
+            this.txtStreetNumber.TabIndex = 3;
+            // 
+            // txtStreetName
+            // 
+            this.txtStreetName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStreetName.Location = new System.Drawing.Point(97, 113);
+            this.txtStreetName.Name = "txtStreetName";
+            this.txtStreetName.Size = new System.Drawing.Size(231, 30);
+            this.txtStreetName.TabIndex = 4;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(193, 49);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(232, 30);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // cmbStreetType
+            // 
+            this.cmbStreetType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStreetType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbStreetType.FormattingEnabled = true;
+            this.cmbStreetType.Items.AddRange(new object[] {
+            "AV",
+            "BV",
+            "CR",
+            "DR",
+            "LN",
+            "RD",
+            "SQ",
+            "ST",
+            "TR"});
+            this.cmbStreetType.Location = new System.Drawing.Point(335, 113);
+            this.cmbStreetType.Name = "cmbStreetType";
+            this.cmbStreetType.Size = new System.Drawing.Size(90, 30);
+            this.cmbStreetType.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(201, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "St #";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(104, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Street Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(341, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Type";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(36, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Province";
+            // 
+            // cmbProvinces
+            // 
+            this.cmbProvinces.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProvinces.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbProvinces.FormattingEnabled = true;
+            this.cmbProvinces.Items.AddRange(new object[] {
+            "AB",
+            "BC",
+            "MB",
+            "NB",
+            "NL",
+            "NS",
+            "NT",
+            "NU",
+            "ON",
+            "PE",
+            "QC",
+            "SK",
+            "YT"});
+            this.cmbProvinces.Location = new System.Drawing.Point(30, 175);
+            this.cmbProvinces.Name = "cmbProvinces";
+            this.cmbProvinces.Size = new System.Drawing.Size(90, 30);
+            this.cmbProvinces.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(146, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "City";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCity.Location = new System.Drawing.Point(139, 175);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(286, 30);
+            this.txtCity.TabIndex = 7;
+            // 
+            // chkPermission
+            // 
+            this.chkPermission.AutoSize = true;
+            this.chkPermission.Location = new System.Drawing.Point(30, 491);
+            this.chkPermission.Name = "chkPermission";
+            this.chkPermission.Size = new System.Drawing.Size(183, 24);
+            this.chkPermission.TabIndex = 16;
+            this.chkPermission.Text = "May we contact you?";
+            this.chkPermission.UseVisualStyleBackColor = true;
+            this.chkPermission.CheckedChanged += new System.EventHandler(this.chkPermission_CheckedChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(37, 342);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(125, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Enter a user name";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(30, 306);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(395, 30);
+            this.txtUserName.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(37, 412);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(69, 17);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(30, 376);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(183, 30);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(231, 412);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(109, 17);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Verify Password";
+            // 
+            // txtPasswordVerify
+            // 
+            this.txtPasswordVerify.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPasswordVerify.Location = new System.Drawing.Point(224, 376);
+            this.txtPasswordVerify.Name = "txtPasswordVerify";
+            this.txtPasswordVerify.Size = new System.Drawing.Size(201, 30);
+            this.txtPasswordVerify.TabIndex = 11;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(37, 278);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(129, 17);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Enter email addess";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(30, 242);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(395, 30);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // contactForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 714);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtPasswordVerify);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.chkPermission);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbProvinces);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbStreetType);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtStreetName);
+            this.Controls.Add(this.txtStreetNumber);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnClear);
+            this.Name = "contactForm";
+            this.Text = "COMP1030 Final Exam Contact Form";
+            this.Load += new System.EventHandler(this.contactForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtStreetNumber;
+        private System.Windows.Forms.TextBox txtStreetName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.ComboBox cmbStreetType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbProvinces;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.CheckBox chkPermission;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtPasswordVerify;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtEmail;
+    }
+}
+
---
+++ finalExamQuestion/Form1.cs
@@ -0,0 +1,215 @@
+﻿// Question 1.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace finalExamQuestion
+{
+    public partial class contactForm : Form
+    {
+        public contactForm()
+        {
+            InitializeComponent();
+        }
+
+        private void contactForm_Load(object sender, EventArgs e)
+        {
+            btnSubmit.Enabled = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            textBoxValidation();
+        }
+
+        private void chkPermission_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkPermission.Checked)
+            {
+                btnSubmit.Enabled = true;
+            }
+            else
+            {
+                btnSubmit.Enabled = false;
+            }
+
+        }
+
+        public void reset()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is TextBox)
+                {
+                    ((TextBox)x).Clear();
+                }
+            }
+
+            foreach (Control y in this.Controls)
+            {
+                if (y is CheckBox)
+                {
+                    ((CheckBox)y).Checked = false;
+                }
+            }
+
+            // Question 5. Uncomment the foreach loop and debug where necessary.
+
+            //foreach (Control z in this.Controls)
+            //{
+            //    if (z is ComboBox)
+            //    {
+            //        cmbStreetType => 0;
+            //        cmbProvinces => 0;
+            //    }
+            //}
+        }
+
+        public void textBoxValidation()
+        {
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text))
+            {
+                MessageBox.Show("Please fill in your first name", "INPUT ERROR");
+            }
+            else if (string.IsNullOrWhiteSpace(txtLastName.Text))
+            {
+                MessageBox.Show("Please fill in your last name", "INPUT ERROR");
+            }
+            else if (string.IsNullOrWhiteSpace(txtStreetNumber.Text))
+            {
+                MessageBox.Show("Please fill in your street number", "INPUT ERROR");
+            }
+            else if (string.IsNullOrWhiteSpace(txtStreetName.Text))
+            {
+                MessageBox.Show("Please fill in your street name", "INPUT ERROR");
+            }
+            else if (string.IsNullOrWhiteSpace(txtCity.Text))
+            {
+                MessageBox.Show("Please fill in your city", "INPUT ERROR");
+            }
+            else if (string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                MessageBox.Show("Please fill in your email address", "INPUT ERROR");
+            }
+            else if (string.IsNullOrWhiteSpace(txtUserName.Text))
+            {
+                MessageBox.Show("Please fill in your user name", "INPUT ERROR");
+            }
+            else if (string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                MessageBox.Show("Please fill in your password", "INPUT ERROR");
+            }
+            else if (string.IsNullOrWhiteSpace(txtPasswordVerify.Text))
+            {
+                MessageBox.Show("Please fill in your password verification", "INPUT ERROR");
+            }
+            else
+            {
+                streetNumValidation();
+                return;
+            }
+        }
+
+        // Question 2
+        // Add number validation to the textbox labelled txtStreetNumber verifying the entry is an integer
+
+        public void streetNumValidation()
+        {
+            streetTypeNotEmpty();
+        }
+
+        public void streetTypeNotEmpty()
+        {
+            if (string.IsNullOrEmpty(cmbStreetType.Text))
+            {
+                MessageBox.Show("Street type is blank", "INPUT ERROR");
+            }
+            else
+            {
+                provincesNotEmpty();
+                return;
+            }
+        }
+        public void provincesNotEmpty()
+        {
+            if (string.IsNullOrEmpty(cmbProvinces.Text))
+            {
+                MessageBox.Show("Provinces field is blank", "INPUT ERROR");
+            }
+            else
+            {
+                emailValidation();
+                return;
+            }
+        }
+
+        private void emailValidation()
+        {
+            if (this.IsValidEmail(txtEmail.Text))
+            {
+                //passwordVerify();
+                output();
+            }
+            else
+            {
+                MessageBox.Show(txtEmail.Text + " is not in a valid email format.", "Invalid Email",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+        // Question 3. Add an if statement that validates that the two text boxes (txtPassword and txtPasswordVerify are the same.
+        // Where the result is true, call the method output (see below).
+        public void passwordVerify()
+        {
+            //output();
+        }
+
+        private bool IsValidEmail(string email)
+        {
+
+            return Regex.IsMatch(txtEmail.Text, @"^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$");
+        }
+
+        // Question 4. Add an array based on the SelectedIndex of the cmbProvinces that will return the full province name
+        // in the message box. The list of the provinces are commented out below and can be accessed by
+        // uncommenting them. The result MUST replace the cmbProvinces output in the MessageBox
+        private void output()
+        {
+            // Alberta British Columbia Manitoba New Brunswick Newfoundland Labrador Nova Scotia Northwest Territories Nunavut Ontario Prince Edward Island Quebec Saskatchewan Yukon
+
+            DialogResult dialogResult = MessageBox.Show("Name: " + "\t\t" + txtFirstName.Text + " " + txtLastName.Text + "\n" +
+            "Address: " + "\t\t" + txtStreetNumber.Text + " " + txtStreetName.Text + " " + cmbStreetType.Text + "\n" +
+            "\t\t" + txtCity.Text + ", " + cmbProvinces.Text + "\n" +
+            "Email: " + "\t\t" + txtEmail.Text + "\n" +
+            "User Name: " + "\t" + txtUserName.Text + "\nPassword:\t" + txtPassword.Text + "\n\nPASSWORD CONFIRMED\n\nDo you wish to close the application?"
+            , "SUBMISSION CONFIRMATION", MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                reset();
+            }
+        }
+    }
+}
+
+
+
+
+
